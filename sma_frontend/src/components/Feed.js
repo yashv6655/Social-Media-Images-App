166ae82ac91acea724fab5e97106446da74e6bbf
Velagapudi, Yash <yvelagap@purdue.edu>
2022-01-22 23:33:06 -0500

    connected feed to pins with loading component

--- sma_frontend/src/components/Feed.js
+++ sma_frontend/src/components/Feed.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 const Feed = () => {
-  return <div>feed</div>;
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+  const [pins, setPins] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+
+  if (loading) {
+    return <Spinner message="Adding new ideas" />;
+  }
+
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
 };
 
 export default Feed;
